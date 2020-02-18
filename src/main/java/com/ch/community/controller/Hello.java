2706456aa47d17be49b03aaeafdb37eaa64bb954
ch529395042 <529395042@qq.com>
2020-02-18 17:28:08 +0800

    git init

---
+++ src/main/java/com/ch/community/controller/Hello.java
@@ -0,0 +1,15 @@
+package com.ch.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+public class Hello {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name",required = false,defaultValue = "123")String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
